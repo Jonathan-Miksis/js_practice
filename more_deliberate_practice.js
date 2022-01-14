8e8dd7319acbe33137e2dd05fd92596974bb0795
Jonathan-Miksis <jmiksis13@gmail.com>
2022-01-14 12:07:23 -0500

    more arrays, nested loops

--- more_deliberate_practice.js
+++ more_deliberate_practice.js
@@ -23,3 +23,45 @@ function strLength(arry) {
 
 console.log(strLength(["hello", "yay", "coding", "fun"]));
 
+// Write a function that accepts an array of numbers and returns a new array with only the even numbers.
+
+function evenNumbers(nums) {
+  var evens = [];
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      evens.push(nums[i]);
+    }
+  }
+  return evens;
+}
+
+console.log(evenNumbers([1, 5, 2, 8, 10, 3]));
+
+
+// Write a function that accepts an array of numbers and returns the smallest number.
+
+function smallestNum(values) {
+  var smallest = values[0];
+  values.forEach(function (value) {
+    if (value < smallest) {
+      smallest = value;
+    }
+  });
+  return smallest;
+}
+
+console.log(smallestNum([7, 3, 0, -9, 15, 8]));
+
+// nested loops practice
+
+// Write a function that accepts an array of numbers, and returns an array of the products of every pair of numbers from the array.
+
+function productArry(arrynums) {
+  var products = [];
+  for (var i = 0; i < arrynums.length - 1; i++) {
+    products.push(arrynums[i] * arrynums[i + 1]);
+  }
+  return products;
+}
+
+console.log(productArry([3, 4, 5, 6]));
